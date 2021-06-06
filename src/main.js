a311e6095f6fe5d647c9b61f1aa423bd9b34b485
Sveinung Tørresdal <sveinung.torresdal@gmail.com>
2021-06-06 12:41:31 +0200

    Project init

---
+++ src/main.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line camelcase, no-undef
+__webpack_nonce__ = window.__webpack_nonce__ = window.btoa(Math.random().toString(36).substr(7));
+
+import { createApp } from 'vue';
+import { router } from './router';
+import App from './App.vue';
+
+// Importing SCSS file.
+import './style/main.scss';
+
+const app = createApp(App);
+
+app.use(router);
+app.mount('#vue');
---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const base = {
+    entry: './src/main.js',
+    devServer: {
+        headers: {
+            'Access-Control-Allow-Origin': 'payex.humany.net'
+        },
+        https: true
+    },
+    devtool: 'cheap-module-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                loader: 'file-loader',
+                options: {
+                    outputPath: 'img/',
+                    name: '[hash:8].[ext]'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'main.css',
+            chunkFilename: '[name].css'
+        }),
+        new VueLoaderPlugin(),
+        new HtmlWebpackPlugin({
+            title: '🔥DEX',
+            //favicon: './src/img/favicon.png',
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            minify: {
+                collapseWhitespace: false,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            }
+        })
+    ],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'main.js',
+        publicPath: '/'
+    }
+};
+
+const development = {
+    ...base,
+    mode: 'development'
+};
+
+module.exports = [development];
